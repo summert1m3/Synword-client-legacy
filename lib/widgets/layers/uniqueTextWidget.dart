5806142554fbfcaadeebeb7407bc973b57532b36
DarkArTes2 <50269308+DarkArTes2@users.noreply.github.com>
2020-10-23 15:35:56 +0300

    Update uniqueTextWidget
    Adding scrolling and ability to copy

--- lib/widgets/layers/uniqueTextWidget.dart
+++ lib/widgets/layers/uniqueTextWidget.dart
@@ -6,6 +6,7 @@ import 'package:synword/types.dart';
 class UniqueTextWidget extends StatelessWidget {
   final Offset _offset;
   final Color _titleColor;
+  final String _uniqueText = 'Исторически сложилось так, что программирование возникло и развивалось как процедурное программирование. В школьных курсах информатики рассматриваются традиционные процедурно-ориентированные языки программирования.';
   final OnPanUpdateCallback _gestureDetectorOnPanUpdateCallback;
   final OnPanEndCallback _gestureDetectorOnPanEndCallback;
   final CloseButtonCallback _closeButtonCallback;
@@ -13,7 +14,7 @@ class UniqueTextWidget extends StatelessWidget {
   final bool _isCloseButtonVisible;
 
   UniqueTextWidget(
-  this._offset,
+    this._offset,
     this._titleColor,
     this._isTitleVisible,
     this._isCloseButtonVisible,
@@ -30,12 +31,11 @@ class UniqueTextWidget extends StatelessWidget {
           SizedBox(
               child: Container(
                 margin: EdgeInsets.all(10),
-                child: Text('Исторически сложилось так, что программирование возникло и развивалось как процедурное программирование. В школьных курсах информатики рассматриваются традиционные процедурно-ориентированные языки программирования.',
-                  style: TextStyle(fontSize: 20, fontFamily: 'Audrey'),
-                ),
+                child: SelectableText(_uniqueText,
+                  style: TextStyle(fontSize: 20, fontFamily: 'Audrey'),),
               ),
               width: MediaQuery.of(context).copyWith().size.width - 20,
-              height: MediaQuery.of(context).copyWith().size.height
+              height: _uniqueText.length / 0.5
           ),
           LayerTitle(
               Text('Unique text', style: TextStyle(fontSize: 25, fontFamily: 'Audrey', fontWeight: FontWeight.bold, color: Colors.black)),
