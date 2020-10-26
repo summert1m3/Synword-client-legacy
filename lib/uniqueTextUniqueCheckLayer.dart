5ed7b1314762c61f60b873c50b71f9c604999b92
DarkArTes2 <50269308+DarkArTes2@users.noreply.github.com>
2020-10-27 01:48:05 +0300

    Fix bugs in layer animations

--- lib/uniqueTextUniqueCheckLayer.dart
+++ lib/uniqueTextUniqueCheckLayer.dart
@@ -8,23 +8,23 @@ import 'types.dart';
 class UniqueTextUniqueCheckLayer extends MovingLayer {
   double _progress;
 
-  UniqueTextUniqueCheckLayer.zero() : super(null, Offset.zero, UniqueCheckTitleColor, null, null, null, false, true, true) {
+  UniqueTextUniqueCheckLayer.zero() : super(null, Offset.zero, UniqueCheckTitleColor, null, null, null, false, true, true, true) {
     _progress = 0;
     build();
   }
 
-  UniqueTextUniqueCheckLayer.common(Offset offset, this._progress) : super(null, offset, UniqueCheckTitleColor, null, null, null, false, true, true) {
+  UniqueTextUniqueCheckLayer.common(Offset offset, this._progress) : super(null, offset, UniqueCheckTitleColor, null, null, null, false, true, true, true) {
     build();
   }
 
-  UniqueTextUniqueCheckLayer(Offset offset, this._progress, Color titleColor, bool isLoadingWidgetEnabled, bool isTitleVisible, bool isCloseButtonVisible, OnPanUpdateCallback onPanUpdateCallback, OnPanEndCallback onPanEndCallback, CloseButtonCallback closeButtonCallback)
-      : super(null, offset, titleColor, onPanUpdateCallback, onPanEndCallback, closeButtonCallback, isLoadingWidgetEnabled, isTitleVisible, isCloseButtonVisible) {
+  UniqueTextUniqueCheckLayer(Offset offset, this._progress, Color titleColor, bool isLoadingWidgetEnabled, bool isMovingEnabled, bool isTitleVisible, bool isCloseButtonVisible, OnPanUpdateCallback onPanUpdateCallback, OnPanEndCallback onPanEndCallback, CloseButtonCallback closeButtonCallback)
+      : super(null, offset, titleColor, onPanUpdateCallback, onPanEndCallback, closeButtonCallback, isLoadingWidgetEnabled, isMovingEnabled, isTitleVisible, isCloseButtonVisible) {
     build();
   }
 
   void build() {
     widget = UniqueCheckWidget(
-        isLoadingWidgetEnabled ? loadingWidget : TextUniqueCheck(offset, _progress),
+        isLoadingWidgetEnabled ? loadingScreen : TextUniqueCheck(offset, _progress),
         offset,
         titleColor,
         isTitleVisible,
